e7dedcc676c1a551ac9ba528a04c1fa5465dd3d0
eliotora <83270395+eliotora@users.noreply.github.com>
2021-04-28 12:49:34 +0200

    Ajout Bonus
    Ajout de l'interaction d'élargissement de la barre avec le Bonus

--- classes/Barre.kt
+++ classes/Barre.kt
@@ -8,10 +8,13 @@ import android.view.MotionEvent
 import kotlin.math.PI
 import kotlin.math.atan
 
-class Barre (x0: Float, y1: Float, width: Float): Parois(x0-width/2, y1, x0+width/2, y1 + 20F) {
+class Barre (x0: Float, y1: Float, width: Float, v: CasseBriqueView): Parois(x0-width/2, y1, x0+width/2, y1 + 50F) {
+    var sprite = RectF(x0-width/2, y1, x0+width/2, y1+20f)
     var x: Float = x0
     val y = y1
     var w = width
+    val view = v
+    var wideT = 0.0
 
 
     init{
@@ -19,8 +22,15 @@ class Barre (x0: Float, y1: Float, width: Float): Parois(x0-width/2, y1, x0+widt
     }
 
     override fun draw(canvas: Canvas) {
-        r = RectF(x-w/2, y, x+w/2, y+20f)
-        canvas.drawRect(r, paint)
+        if (wideT > 0.0) {
+            r = RectF(x-w*1.5f/2, y, x+w*1.5f/2, y+50f)
+            sprite = RectF(x-w*1.5f/2, y, x+w*1.5f/2, y+20f)
+        }
+        else {
+            r = RectF(x-w/2, y, x+w/2, y+50f)
+            sprite = RectF(x-w/2, y, x+w/2, y+20f)
+        }
+        canvas.drawRect(sprite, paint)
     }
 
     fun move(motion: MotionEvent) {
@@ -34,36 +44,37 @@ class Barre (x0: Float, y1: Float, width: Float): Parois(x0-width/2, y1, x0+widt
         }
     }
 
-    override fun gereBalle(b: Balle): Boolean {
+    override fun gereBalle(b: Balle, dTime: Double): Boolean {
         var flag = false
         if (RectF.intersects(r, b.balle)){
             flag = true
             var new_dir = 0.0
             val alpha = atan((b.balle.centerY()- r.centerY())/(b.balle.centerX()-r.centerX())).toDouble()
             if (b.balle.centerX() > r.centerX()) {
-                new_dir = alpha
+                new_dir = -alpha
             }
             if (b.balle.centerX() < r.centerX()) {
                 new_dir = PI - alpha
             }
             if (b.balle.centerX() == r.centerX()) {
                 if (b.balle.centerY() - r.centerY() > 0) {
-                    new_dir = PI/2
-                }
-                else {
                     new_dir = -PI/2
                 }
+                else {
+                    new_dir = PI/2
+                }
             }
-            b.barre_interract(new_dir)
+            if (new_dir>= 0) view.text = "Angle = " + new_dir.toString()
+            b.barre_interract(new_dir, dTime)
         }
         return flag
     }
 
-    /*fun activeBonus(bo: Bonus) {
-        if (RectF.intersects(barre, bo.icon)) {
-        //une icone par bonus
-        icon.active //todo
-        }
-    }*/
+    fun update(dTime: Double) {
+        wideT -= dTime
+    }
 
+    fun wide() {
+        wideT = 10.0
+    }
 }
